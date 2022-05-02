376de95691d952ababbaad1a24b912fba9f10259
Steve Alomia <stevealomia@yahoo.com>
2022-05-02 15:15:25 -0400

    commit tree 2ca03f325800742ace4bfbd99f7a1cfedd375133

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+
+    def sit
+        puts "The Dog is sitting"
+    end
+
+
+end
+
+rufo = Dog.new 
+rufo.bark 
+
+kenai = Dog.new
+kenai.bark 
+
